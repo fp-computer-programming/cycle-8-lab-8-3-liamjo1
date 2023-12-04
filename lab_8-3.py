56888d26ceb394f6081e644de70d5bbb20735695
p24lohara <p24lohara@fairfieldprep.org>
2023-12-03 22:30:16 -0500

    lab 8-3

--- lab_8-3.py
+++ lab_8-3.py
@@ -1,5 +1,5 @@
 """
-Create a Python file named lab_8-1.py
+
 
 *** You must write a comment for every chunk of code you write from now on along with your author tag***
 Create a Python file named lab_8-3.py
@@ -13,3 +13,19 @@ def sum_to(n):
 
 The function should be able to have an integer passed to it and return the sum of all the values from 1 to that integer
 """
+def sum_to(n):
+    
+    # Initialize variables
+    total = 0
+    i = 1
+
+    # Use a while loop to iterate through numbers until "i" is larger than "n"
+    while i <= n:
+        total += i
+        i += 1
+
+    return total
+
+# Find the result of the function through an example
+result = sum_to(5)
+print(f"The sum of values from 1 to 5 is: {result}")
